4e61ef44087f467b94c1b9727ec59996b1cb10ed
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2025-03-16 16:13:24 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue'
+import { RouterView } from 'vue-router'
+import FooterComponent from './components/FooterComponent.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+  <RouterView />
+  <FooterComponent />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log('HomeView mounted')
+})
+</script>
+
+<template>
+  <main>
+    Main
+  </main>
+</template>
