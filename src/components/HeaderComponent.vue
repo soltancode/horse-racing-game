06f79a7bc8a59bc54e49e6c83aaccfb4b288ac08
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2025-03-16 20:41:59 +0400

    Update interface

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,24 @@
-<script setup>
-</script>
-
 <template>
-    <div>
-        Header
-    </div>
+    <header class="bg-white">
+        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <div class="flex lg:flex-1">
+                <a href="#" class="-m-1.5 p-1.5">
+                    <div class="flex flex-col">
+                        <span class="text-lg font-bold">Horse Racing Game</span>
+                        <span class="text-xs text-gray-500">Developed by soltancode</span>
+                    </div>
+                    <!-- <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt=""> -->
+                </a>
+            </div>
+            <div class="hidden lg:flex lg:gap-x-12">
+                <a href="https://github.com/soltancode" target="_blank" class="text-sm/6 font-semibold text-gray-900">GitHub</a>
+                <a href="https://soltancode.com" target="_blank" class="text-sm/6 font-semibold text-gray-900">Developer</a>
+                <a href="https://linkedin.com/in/soltancode" target="_blank" class="text-sm/6 font-semibold text-gray-900">LinkedIn</a>
+            </div>
+            <div class="flex flex-col lg:flex-row lg:flex-1 lg:justify-end gap-2">
+                <a href="#" class="text-sm font-semibold text-gray-900 bg-gray-300/50 hover:bg-gray-300 px-3 py-1 rounded-full">Generate Program</a>
+                <a href="#" class="text-sm font-semibold text-white bg-blue-500 hover:bg-gray-300 px-3 py-1 rounded-full">Start / Pause</a>
+            </div>
+        </nav>
+    </header>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,127 @@ onMounted(() => {
 
 <template>
   <main>
-    Main
+    <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+      <div class="grid grid-cols-5 gap-5 w-full">
+        <div class="overflow-scroll bg-gray-50 py-3 px-2 rounded-lg">
+          <span class="font-semibold text-xl">Horse List</span>
+          <div class="max-h-96 overflow-y-scroll">
+            <table class="min-w-full overflow-scroll divide-y divide-gray-300 mt-4">
+              <thead>
+                <tr>
+                  <th scope="col" class="text-left py-2 text-xs font-semibold text-gray-900">Name</th>
+                  <th scope="col" class="text-left py-2 text-xs font-semibold text-gray-900">Condition</th>
+                  <th scope="col" class="text-left py-2 text-xs font-semibold text-gray-900">Color</th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200">
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Grace Hopper</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">80</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Red</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+                <tr>
+                  <td class="whitespace-nowrap py-2 text-left text-xs font-medium text-gray-900">Soltan Code</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">90</td>
+                  <td class="whitespace-nowrap py-2 text-left text-xs text-gray-500">Yellow</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="col-span-2">2</div>
+        <div class="col-span-1">3</div>
+      </div>
+    </div>
   </main>
 </template>
