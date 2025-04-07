e5ada76b71783832e9cc1cfb3e5f768422f92aed
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2025-04-07 12:58:37 +0400

    Update animation.js

--- src/constants/animation.js
+++ src/constants/animation.js
@@ -1,4 +1,11 @@
+// Number of frames in the sprite sheet
 export const SPRITE_FRAME_COUNT = 12;
+
+// Width of each frame in the sprite sheet (px)
 export const SPRITE_FRAME_WIDTH = 60;
+
+// Height of each frame in the sprite sheet (px)
 export const SPRITE_FRAME_HEIGHT = 40;
+
+// Duration between each frame switch (ms)
 export const SPRITE_INTERVAL = 100;
