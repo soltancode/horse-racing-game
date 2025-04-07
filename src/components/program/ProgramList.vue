16b618077947303b1832e25411aa3694fc5ba7f6
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2025-04-07 16:16:40 +0400

    Update ProgramList.vue

--- src/components/program/ProgramList.vue
+++ src/components/program/ProgramList.vue
@@ -2,16 +2,14 @@
 import { useRaceStore } from '@/stores/raceStore.js';
 import ProgramRound from '@/components/program/round/ProgramRound.vue';
 
+// Access round data from the race store
 const raceStore = useRaceStore();
 </script>
 
 <template>
   <div class="col-span-1 bg-gray-50 p-4 rounded-lg horse-list overflow-scroll">
     <span class="font-semibold text-lg">Program</span>
-    <ProgramRound 
-      v-for="(round, index) in raceStore.rounds" 
-      :key="index" 
-      :round="round"
-    />
+    <!-- Render each round using the ProgramRound component -->
+    <ProgramRound v-for="round in raceStore.rounds" :key="round.round" :round="round"/>
   </div>
 </template>
